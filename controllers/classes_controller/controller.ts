0bab0f54871badd5160fc86a87e02e2fb5cea942
Alexys LAURENT <alexyslaurent.442@gmail.com>
2025-09-28 18:31:22 +0200

    GET /api/classes/:idClasse
    Get one class

---
+++ controllers/classes_controller/controller.ts
@@ -0,0 +1,23 @@
+import Class from '#models/class'
+import { inject } from '@adonisjs/core'
+import type { HttpContext } from '@adonisjs/core/http'
+import AbstractController from '../abstract_controller.js'
+import { onlyIdClassWithExistsValidator } from './validator.js'
+
+export default class ClassesController extends AbstractController {
+  constructor() {
+    super()
+  }
+
+  /**
+   * Get one class
+   */
+  @inject()
+  public async getOneClass({ params }: HttpContext) {
+    const valid = await onlyIdClassWithExistsValidator.validate(params)
+    const theClass = await Class.findOrFail(valid.idClass)
+    return this.buildJSONResponse({
+      data: theClass,
+    })
+  }
+}
