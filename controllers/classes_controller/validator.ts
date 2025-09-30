384124b58dd15b6a37f42fabe91fd6c93c9e9958
neal <nealbristol2002@gmail.com>
2025-09-30 15:06:07 +0200

    add delete method for class only with account type = Admin

--- controllers/classes_controller/validator.ts
+++ controllers/classes_controller/validator.ts
@@ -1,10 +1,24 @@
 import vine from '@vinejs/vine'
 
+
 export const onlyIdClassWithExistsValidator = vine.compile(
   vine.object({
     idClass: vine.string().exists(async (db, value) => {
       const row = await db.from('classes').where('id_class', value).first()
       return row ? true : false
+    }),
+  })  
+)
+
+export const onlyIdAdminAndClassWithExistsValidator = vine.compile(
+  vine.object({
+    idAdmin: vine.string().exists(async (db, value) => {
+      const row = await db.from('users').where('id_user', value).andWhere('account_type', 'admin').first()
+      return !!row
+    }),
+    idClass: vine.string().exists(async (db, value) => {
+      const row = await db.from('classes').where('id_class', value).first()
+      return !!row
     }),
   })
 )
