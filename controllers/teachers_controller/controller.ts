c55449244fe5b740e9a43dc852ca5595716a175b
lou Wargnier <louisbw78@gmail.com>
2025-10-04 17:42:04 +0200

    [ADD] get teacher with id

--- controllers/teachers_controller/controller.ts
+++ controllers/teachers_controller/controller.ts
@@ -1,7 +1,18 @@
+import User from '#models/user'
+import { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
+import { onlyIdTeacherWithExistsValidator } from './validator.js'
 
-export default class StudentsController extends AbstractController {
+export default class TeachersController extends AbstractController {
   constructor() {
     super()
+  }
+
+  public async getOneTeacher({ params }: HttpContext) {
+    const valid = await onlyIdTeacherWithExistsValidator.validate(params)
+    const theTeacher = await User.findOrFail(valid.idTeacher)
+    return this.buildJSONResponse({
+      data: theTeacher,
+    })
   }
 }
--- controllers/teachers_controller/validator.ts
+++ controllers/teachers_controller/validator.ts
@@ -1,1 +1,14 @@
-// import vine from '@vinejs/vine'
+import vine from '@vinejs/vine'
+
+export const onlyIdTeacherWithExistsValidator = vine.compile(
+  vine.object({
+    idTeacher: vine.string().exists(async (db, value) => {
+      const row = await db
+        .from('users')
+        .where('id_user', value)
+        .andWhere('account_type', 'teacher')
+        .first()
+      return row ? true : false
+    }),
+  })
+)
