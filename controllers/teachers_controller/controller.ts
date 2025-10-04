803f147d75af56f18335f76c28da3989ba677110
lou Wargnier <louisbw78@gmail.com>
2025-10-04 19:18:59 +0200

    [ADD] create teacher

--- controllers/teachers_controller/controller.ts
+++ controllers/teachers_controller/controller.ts
@@ -1,7 +1,23 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
+import { createTeacherValidator } from './validator.js'
 
-export default class StudentsController extends AbstractController {
+export default class TeachersController extends AbstractController {
   constructor() {
     super()
+  }
+
+  public async createTeacher({ request }: HttpContext) {
+    const content = await createTeacherValidator.validate(request.body())
+    const teacher = await User.create({
+      lastName: content.lastName,
+      name: content.name,
+      email: content.email,
+      password: content.password,
+      avatarPath: content.avatarPath ?? null,
+      accountType: 'teacher',
+    })
+    return this.buildJSONResponse({ data: teacher })
   }
 }
--- controllers/teachers_controller/validator.ts
+++ controllers/teachers_controller/validator.ts
@@ -1,1 +1,11 @@
-// import vine from '@vinejs/vine'
+import vine from '@vinejs/vine'
+
+export const createTeacherValidator = vine.compile(
+  vine.object({
+    lastName: vine.string().trim().maxLength(100),
+    name: vine.string().trim().maxLength(100),
+    email: vine.string().email().trim(),
+    password: vine.string().minLength(6),
+    avatarPath: vine.string().optional(),
+  })
+)
