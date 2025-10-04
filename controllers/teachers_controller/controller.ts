e7821c5b6bce67dfafcba133ef9e38334c5fc933
lou Wargnier <louisbw78@gmail.com>
2025-10-04 19:31:26 +0200

    [ADD] delete teacher

--- controllers/teachers_controller/controller.ts
+++ controllers/teachers_controller/controller.ts
@@ -1,7 +1,16 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
+import { onlyIdTeacherWithExistsValidator } from './validator.js'
 
-export default class StudentsController extends AbstractController {
+export default class TeachersController extends AbstractController {
   constructor() {
     super()
+  }
+
+  public async deleteTeacher({ params }: HttpContext) {
+    const teacher = await onlyIdTeacherWithExistsValidator.validate(params)
+    await User.query().where('id_user', teacher.idTeacher).delete()
+    return this.buildJSONResponse({ data: teacher })
   }
 }
