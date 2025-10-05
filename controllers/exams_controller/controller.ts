9893caecb93cf5b03134f3d044ab5f9cffd6dc60
lou Wargnier <louisbw78@gmail.com>
2025-10-05 11:43:32 +0200

    [ADD] post exams

--- controllers/exams_controller/controller.ts
+++ controllers/exams_controller/controller.ts
@@ -1,7 +1,24 @@
+import Exam from '#models/exam'
+import type { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
+import { createExamValidator } from './validator.js'
+import { DateTime } from 'luxon'
 
-export default class QuestionsController extends AbstractController {
+export default class ExamController extends AbstractController {
   constructor() {
     super()
+  }
+
+  public async createExam({ request }: HttpContext) {
+    const content = await createExamValidator.validate(request.body())
+    const exam = await Exam.create({
+      title: content.title,
+      desc: content.desc,
+      time: content.time,
+      imagePath: content.imagePath ?? null,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    })
+    return this.buildJSONResponse({ data: exam })
   }
 }
--- controllers/exams_controller/validator.ts
+++ controllers/exams_controller/validator.ts
@@ -1,1 +1,10 @@
-// import vine from '@vinejs/vine'
+import vine from '@vinejs/vine'
+
+export const createExamValidator = vine.compile(
+  vine.object({
+    title: vine.string().trim().maxLength(100),
+    desc: vine.string().trim().maxLength(255),
+    time: vine.number().min(0),
+    imagePath: vine.string().optional(),
+  })
+)
