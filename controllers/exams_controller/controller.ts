aa1bb57a2c1b55e4a7e1e1816b1654d48d07e7d1
lou Wargnier <louisbw78@gmail.com>
2025-10-05 11:48:41 +0200

    [ADD] get exam by id

--- controllers/exams_controller/controller.ts
+++ controllers/exams_controller/controller.ts
@@ -1,7 +1,18 @@
+import Exam from '#models/exam'
+import { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
+import { onlyIdExamWithExistsValidator } from './validator.js'
 
 export default class QuestionsController extends AbstractController {
   constructor() {
     super()
+  }
+
+  public async getOneExam({ params }: HttpContext) {
+    const valid = await onlyIdExamWithExistsValidator.validate(params)
+    const theExam = await Exam.findOrFail(valid.idExam)
+    return this.buildJSONResponse({
+      data: theExam,
+    })
   }
 }
