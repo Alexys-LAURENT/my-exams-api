2e6ca837022332eb7f41942ced665ddeea47f4d2
lou Wargnier <louisbw78@gmail.com>
2025-10-05 11:28:19 +0200

    [ADD] modify user

--- controllers/teachers_controller/controller.ts
+++ controllers/teachers_controller/controller.ts
@@ -1,7 +1,26 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
+import { updateTeacherValidator, onlyIdTeacherWithExistsValidator } from './validator.js'
 
-export default class StudentsController extends AbstractController {
+export default class TeachersController extends AbstractController {
   constructor() {
     super()
+  }
+
+  public async updateTeacher({ request, params }: HttpContext) {
+    const content = await updateTeacherValidator.validate(request.body())
+    const valid = await onlyIdTeacherWithExistsValidator.validate(params)
+    const teacher = await User.findOrFail(valid.idTeacher)
+
+    if (content.lastName) teacher.lastName = content.lastName
+    if (content.name) teacher.name = content.name
+    if (content.email) teacher.email = content.email
+    if (content.avatarPath) teacher.avatarPath = content.avatarPath
+    if (content.password) teacher.password = content.password
+
+    await teacher.save()
+
+    return this.buildJSONResponse({ data: teacher })
   }
 }
--- controllers/teachers_controller/validator.ts
+++ controllers/teachers_controller/validator.ts
@@ -1,1 +1,24 @@
-// import vine from '@vinejs/vine'
+import vine from '@vinejs/vine'
+
+export const onlyIdTeacherWithExistsValidator = vine.compile(
+  vine.object({
+    idTeacher: vine.string().exists(async (db, value) => {
+      const row = await db
+        .from('users')
+        .where('id_user', value)
+        .andWhere('account_type', 'teacher')
+        .first()
+      return row ? true : false
+    }),
+  })
+)
+
+export const updateTeacherValidator = vine.compile(
+  vine.object({
+    lastName: vine.string().trim().maxLength(100).optional(),
+    name: vine.string().trim().maxLength(100).optional(),
+    email: vine.string().email().trim().optional(),
+    password: vine.string().minLength(6).optional(),
+    avatarPath: vine.string().optional(),
+  })
+)
