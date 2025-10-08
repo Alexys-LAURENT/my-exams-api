057dac69b2cbecdb7f253f32cf2af90a4e9d2d6a
lou Wargnier <louisbw78@gmail.com>
2025-10-08 22:13:24 +0200

    [FIX] ma connerie again

--- controllers/teachers_controller/validator.ts
+++ controllers/teachers_controller/validator.ts
@@ -1,10 +1,1 @@
-import vine from '@vinejs/vine'
-
-export const onlyIdExamWithExistsValidator = vine.compile(
-  vine.object({
-    idExam: vine.string().exists(async (db, value) => {
-      const row = await db.from('exams').where('id_exam', value).first()
-      return row ? true : false
-    }),
-  })
-)
+// import vine from '@vinejs/vine'
