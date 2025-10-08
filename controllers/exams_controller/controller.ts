3b5e94ec954106269490cab0675c56cb63c5d10f
neal <neal.bristol@mediashcool.me>
2025-10-08 11:35:58 +0200

    fixed name controller

--- controllers/exams_controller/controller.ts
+++ controllers/exams_controller/controller.ts
@@ -1,8 +1,4 @@
-import Class from '#models/class'
 import AbstractController from '../abstract_controller.js'
-import { onlyIdClassWithExistsValidator } from '../classes_controller/validator.js'
-import type { HttpContext } from '@adonisjs/core/http'
-import { DateTime } from 'luxon'
 
 export default class ExamsController extends AbstractController {
   constructor() {
