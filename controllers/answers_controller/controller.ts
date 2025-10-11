b2e9d93fb1ba92155fc5d8051e37bcd318b20dea
lou Wargnier <louisbw78@gmail.com>
2025-10-11 15:06:03 +0200

    [FIX] is_correct champ in the data return

--- controllers/answers_controller/controller.ts
+++ controllers/answers_controller/controller.ts
@@ -13,6 +13,7 @@ export default class AnswersController extends AbstractController {
     const answers = await Answer.query()
       .where('id_question', idQuestion)
       .andWhere('id_exam', idExam)
+      .select(['id_answer', 'answer', 'created_at', 'updated_at'])
     return this.buildJSONResponse({ data: answers })
   }
 }
