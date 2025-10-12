eb430c431f70ff893e2810856f95e54702cc57f2
neal <neal.bristol@mediashcool.me>
2025-10-12 18:22:59 +0200

    correction

--- controllers/teachers_controller/controller.ts
+++ controllers/teachers_controller/controller.ts
@@ -2,46 +2,28 @@ import AbstractController from '../abstract_controller.js'
 import Class from '#models/class'
 import type { HttpContext } from '@adonisjs/core/http'
 import { classTeacherParamsValidator } from './validator.js'
+import UnauthorizedException from '#exceptions/un_authorized_exception'
 
 export default class TeachersController extends AbstractController {
   constructor() {
     super()
   }
 
-  public async putTeacherToClass({ params, response, auth }: HttpContext) {
-    try {
-      const user = auth.user
-      if (!user || user.accountType !== 'admin') {
-        return response.forbidden(this.buildJSONResponse({
-          message: 'Seuls les administrateurs peuvent ajouter un professeur à une classe'
-        }))
-      }
-
-      const validatedParams = await classTeacherParamsValidator.validate(params)
-      const { idClass, idTeacher } = validatedParams
-
-      const classInstance = await Class.findOrFail(idClass)
-
-      await classInstance.load('teachers', (query) => {
-        query.where('id_user', idTeacher)
-      })
-
-      if (classInstance.teachers.length > 0) {
-        return response.badRequest(this.buildJSONResponse({
-          message: 'Ce professeur est déjà associé à cette classe'
-        }))
-      }
-
-      await classInstance.related('teachers').attach([idTeacher])
-
-      return this.buildJSONResponse({
-        message: 'Professeur associé à la classe avec succès'
-      })
-    } catch (error) {
-      console.error(error)
-      return response.internalServerError(this.buildJSONResponse({
-        message: 'Erreur interne du serveur'
-      }))
+  public async putTeacherToClass({ params, auth }: HttpContext) {
+    const user = auth.user
+    if (!user || user.accountType !== 'admin') {
+      throw new UnauthorizedException('Seuls les administrateurs peuvent ajouter un professeur à une classe')
     }
+
+    const validatedParams = await classTeacherParamsValidator.validate(params)
+    const { idClass, idTeacher } = validatedParams
+
+    const classInstance = await Class.findOrFail(idClass)
+
+    await classInstance.related('teachers').attach([idTeacher])
+
+    return this.buildJSONResponse({
+      message: 'Professeur associé à la classe avec succès'
+    })
   }
 }
