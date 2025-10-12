a5895bc3c481f11dde8888185e1b5d8d5cb0b27a
neal <neal.bristol@mediashcool.me>
2025-10-12 18:32:53 +0200

    correction

--- controllers/exams_controller/controller.ts
+++ controllers/exams_controller/controller.ts
@@ -3,57 +3,36 @@ import Class from '#models/class'
 import Exam from '#models/exam'
 import type { HttpContext } from '@adonisjs/core/http'
 import { classExamParamsValidator } from './validator.js'
+import UnAuthorizedException from '#exceptions/un_authorized_exception'
 
 export default class ExamsController extends AbstractController {
   constructor() {
     super()
   }
 
-  public async deleteExamFromClass({ params, response, auth }: HttpContext) {
-    try {
-
-      const user = auth.user
-      if (!user || (user.accountType !== 'teacher' && user.accountType !== 'admin')) {
-        return response.forbidden(this.buildJSONResponse({
-          message: 'Seuls les professeurs et administrateurs peuvent supprimer un examen d\'une classe'
-        }))
-      }
-
-      const validatedParams = await classExamParamsValidator.validate(params)
-      const { idClass, idExam } = validatedParams
-
-      const classInstance = await Class.findOrFail(idClass)
-      
-      await classInstance.load('exams', (query) => {
-        query.where('exams.id_exam', idExam)
-      })
-
-      if (classInstance.exams.length === 0) {
-        // L'examen n'étant pas associé, on considère que c'est déjà "supprimé"
-        return this.buildJSONResponse({
-          message: 'Examen déjà dissocié de la classe ou jamais associé'
-        })
-      }
-
-      if (user.accountType === 'teacher') {
-        const exam = await Exam.findOrFail(idExam)
-        if (exam.idTeacher !== user.idUser) {
-          return response.forbidden(this.buildJSONResponse({
-            message: 'Vous ne pouvez supprimer que vos propres examens'
-          }))
-        }
-      }
-
-      await classInstance.related('exams').detach([idExam])
-
-      return this.buildJSONResponse({
-        message: 'Examen retiré de la classe avec succès'
-      })
-    } catch (error) {
-      console.error(error)
-      return response.internalServerError(this.buildJSONResponse({
-        message: 'Erreur interne du serveur'
-      }))
+  public async deleteExamFromClass({ params, auth }: HttpContext) {
+    const user = auth.user
+    if (!user || (user.accountType !== 'teacher' && user.accountType !== 'admin')) {
+      throw new UnAuthorizedException('Seuls les professeurs et administrateurs peuvent désassocier un examen d\'une classe')
     }
+
+    const validatedParams = await classExamParamsValidator.validate(params)
+    const { idClass, idExam } = validatedParams
+
+    const classInstance = await Class.findOrFail(idClass)
+    
+    if (user.accountType === 'teacher') {
+      const exam = await Exam.findOrFail(idExam)
+      if (exam.idTeacher !== user.idUser) {
+        throw new UnAuthorizedException('Vous ne pouvez désassocier que vos propres examens')
+      }
+    }
+
+    // Association directe sans vérification préalable
+    await classInstance.related('exams').detach([idExam])
+
+    return this.buildJSONResponse({
+      message: 'Examen désassocié de la classe avec succès'
+    })
   }
 }
