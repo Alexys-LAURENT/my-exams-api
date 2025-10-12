7d85c6fec299663edb65c520ee26f784b2077133
neal <neal.bristol@mediashcool.me>
2025-10-12 20:03:35 +0200

    init post /api/degrees

--- controllers/degrees_controller/controller.ts
+++ controllers/degrees_controller/controller.ts
@@ -1,7 +1,10 @@
 import Class from '#models/class'
+import Degree from '#models/degree'
 import type { HttpContext } from '@adonisjs/core/http'
 import AbstractController from '../abstract_controller.js'
 import { onlyIdClassWithExistsValidator } from '../classes_controller/validator.js'
+import { createDegreeValidator } from './validator.js'
+import UnAuthorizedException from '#exceptions/un_authorized_exception'
 
 export default class DegreesController extends AbstractController {
   constructor() {
@@ -15,6 +18,26 @@ export default class DegreesController extends AbstractController {
     await theClass.load('degree')
     return this.buildJSONResponse({
       data: theClass.degree,
+    })
+  }
+  
+
+  public async createDegree({ request, auth }: HttpContext) {
+
+    const user = auth.user
+    if (!user || user.accountType !== 'admin') {
+      throw new UnAuthorizedException('Seuls les administrateurs peuvent créer des diplômes')
+    }
+
+    const data = await createDegreeValidator.validate(request.body())
+    
+    const degree = await Degree.create({
+      name: data.name
+    })
+
+    return this.buildJSONResponse({
+      message: 'Diplôme créé avec succès',
+      data: degree
     })
   }
 }
