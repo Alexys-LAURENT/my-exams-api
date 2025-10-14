daf649645594cee1f4ceff6ea0bf6e85762cc62b
Alexys LAURENT <alexyslaurent.442@gmail.com>
2025-10-14 14:44:27 +0200

    advanced seeder

--- database/factories/user_factory.ts
+++ database/factories/user_factory.ts
@@ -4,18 +4,6 @@ import factory from '@adonisjs/lucid/factories'
 export const UserFactory = factory
   .define(User, async ({ faker }) => {
     return {
-      // about: faker.lorem.paragraph(),
-      // acceptNewMessageFrom: faker.helpers.arrayElement(['all', 'nobody', 'following']),
-      // accountType: 'student',
-      // alias: faker.internet.username(),
-      // darkMode: faker.datatype.boolean(),
-      // email: faker.internet.email(),
-      // isEmailVerified: true,
-      // isOnboardingCompleted: true,
-      // language: 'fr-FR' as 'fr-FR' | 'en-US' | 'en-GB' | 'es-ES',
-      // password: 'Secret123',
-      // username: faker.internet.username(),
-      // website: faker.internet.url(),
       lastName: faker.person.lastName(),
       name: faker.person.firstName(),
       email: faker.internet.email(),
